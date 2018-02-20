f788a66364390056db9543e0cc874ecd2d366e74
Cj-bc <cj-bc@y-modify.org>
2018-02-20 17:09:01 +0900

    Add: output title logo and info

--- 54ysh.sh
+++ 54ysh.sh
@@ -6,3 +6,24 @@
 # 
 # This software is released under MIT License.
 # http://opensource.org/license/mit-license.php
+
+
+# Logo and info
+cat <<EOT
+
+=======     ===              ======  ==
+==         = ==             ==       ==
+==        =  ==             ==       ==  ==
+=======  =   ==              =====   == =  ==
+     == =========  =   ==        ==  ===    ==
+     ==      ==     =  ==        ==  ==     ==
+=======      ==      ====   =====    ==     ==
+                       ==
+                      ==
+                     ==               by Cj-bc
+
+speaking tool for mac.  ver.0.0.1
+
+Type here
+>
+EOT
