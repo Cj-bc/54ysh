efea1c4d336b88589fe3b28ba518a2ec04d15d39
Cj-bc <cj-bc@y-modify.org>
2018-02-20 16:40:28 +0900

    Add: make main script file, 54ysh.sh

---
+++ 54ysh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# 54ysh -- a shell-like speaking tool for mac
+#
+# copyright (c) 2018 Cj-bc
+# 
+# This software is released under MIT License.
+# http://opensource.org/license/mit-license.php
