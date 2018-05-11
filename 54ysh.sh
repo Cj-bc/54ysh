e2ed3e787443117c6c44828b077296960050cd1f
coord.e <me@coord-e.com>
2018-05-11 21:51:34 +0900

    Use wrapper if `say` could not be found

--- 54ysh.sh
+++ 54ysh.sh
@@ -7,6 +7,16 @@
 # This software is released under MIT License.
 # http://opensource.org/license/mit-license.php
 
+type say >/dev/null 2>&1
+say_found=$?
+
+function say_impl() {
+  if [ $say_found -eq 0 ]; then
+    say $@
+  else
+    ./say_wrapper.sh $@
+  fi
+}
 
 if [ -p /dev/stdin ]
 then
@@ -15,9 +25,9 @@ then
 #    userInput=$(cat -)
     if [ "$(echo "$userInput" | nkf -g)" = "ASCII" ]
     then
-      say -v alex "$userInput"
+      say_impl -v alex "$userInput"
     else
-      say -v kyoko "$userInput"
+      say_impl -v kyoko "$userInput"
     fi
   done
   if [ -p /dev/stdout ]
@@ -54,9 +64,9 @@ while [ "$userInput" != ":q" ]
 do
   if [ "$(echo $userInput | nkf -g)" = "ASCII" ]
   then
-    say -v alex $userInput
+    say_impl -v alex $userInput
   else
-    say -v kyoko $userInput
+    say_impl -v kyoko $userInput
   fi
   
   read userInput
