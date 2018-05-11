7c5f62c116f5d5ac230093c4a2578e10cb5d06cb
coord.e <me@coord-e.com>
2018-05-11 22:08:32 +0900

    Suppress stdout/stderr of speech commands

--- say_wrapper.sh
+++ say_wrapper.sh
@@ -31,12 +31,12 @@ fi
 
 tmpfile=$(mktemp)
 if [ $language = "en" ]; then
-  espeak -w $tmpfile $@
+  espeak -w $tmpfile $@ >/dev/null 2>&1
 elif [ $language = "ja" ]; then
   open_jtalk \
     -m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice \
     -x /var/lib/mecab/dic/open-jtalk/naist-jdic \
-    -ow $tmpfile <<< $@
+    -ow $tmpfile <<< $@ >/dev/null 2>&1
 else
   echo "Language '$language' isn't supported"
   rm $tmpfile
